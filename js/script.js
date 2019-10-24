4d5017d42be231808e019e48b0072301c7e094f0
Kamila Drewelus <kamila.drewelus@gmail.com>
2019-10-24 19:40:03 +0200

    add 2,3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let computerMove = `kamien`;
-let playerMove = `nożyczki`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+  playerMove = 'papier';
+}
+else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
